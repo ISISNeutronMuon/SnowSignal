0f8d6c52fc0186399da61bdde6ace7bb99c07582
Ivan Finch <ivan.finch@stfc.ac.uk>
2024-05-19 10:33:10 +0100

    Fix resource leak (UDP sockets)

--- src/udp_relay_transmit.py
+++ src/udp_relay_transmit.py
@@ -118,8 +118,8 @@ class UDPRelayTransmit:
                 "Send to (%s, %i) message: %r",
                 remote_relay, self.remote_port, relay_payload,
             )
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.sendto(relay_payload, (str(remote_relay), self.remote_port))
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                s.sendto(relay_payload, (str(remote_relay), self.remote_port))
 
     def _send_to_relays_packet(self, packet: scapy.packet.Packet):
         """
@@ -136,8 +136,8 @@ class UDPRelayTransmit:
                 "Send to (%s, %i) message: %r",
                 remote_relay, self.remote_port, pkt_raw,
             )
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.sendto(pkt_raw, (str(remote_relay), self.remote_port))
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                s.sendto(pkt_raw, (str(remote_relay), self.remote_port))
 
         # # Check the packet source. If broadcast a packet with this source in the last
         # # second then we shouldn't do so again
