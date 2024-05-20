452de62a8df0d6ca6c2afc149328222e8d23b5e6
Ivan Finch <ivan.finch@stfc.ac.uk>
2024-05-20 15:38:31 +0100

    Test swarmmode detection

--- test/test_snowsignal.py
+++ test/test_snowsignal.py
@@ -1,15 +1,23 @@
 """ Tests for the snowsignal file """
-
+import os
 import unittest
-from unittest.mock import  patch
+from unittest.mock import patch
 import unittest.mock
 
 from src import snowsignal
 
 class TestSnowSignal(unittest.IsolatedAsyncioTestCase):
+    """ Test the functions in snowsignal.py """
 
-    @patch('asyncio.sleep')
-    async def test_main(self, mock_asyncio_sleep, ):
+    async def test_main(self):
         """ See if main executes without any problems! """
 
         await snowsignal.main('--log-level=error', loop_forever=False)
+
+    def test_is_swarmmode(self):
+        """ Test swarmmode detection """
+
+        self.assertFalse(snowsignal.is_swarmmode())
+
+        with patch.dict(os.environ, {"SERVICENAME": "something"}, clear=True):
+            self.assertTrue(snowsignal.is_swarmmode())
